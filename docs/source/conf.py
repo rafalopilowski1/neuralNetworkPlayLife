46663460cd3610b5ba9910162507193e9847ff08
Rafał Opiłowski <rafalopilowski1@outlook.com>
2023-06-23 13:58:06 +0200

    feat: Sphinx AutoAPI generation

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Neural Network, Play Life!'
+copyright = '2023, Rafał Opiłowski'
+author = 'Rafał Opiłowski'
+release = '1.0.0'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["autoapi.extension"]
+
+autoapi_type = 'python'
+autoapi_dirs = ['../../lib', '../../models', '../../views', '../../controller']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
